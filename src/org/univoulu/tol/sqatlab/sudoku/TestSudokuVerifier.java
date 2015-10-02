26ae76e361ee7167cfc4195734e40978a8c1173f
somename <someemail>
2015-10-02 12:17:33 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -16,17 +16,6 @@ public class TestSudokuVerifier {
 		
 	}
 	
-	/*
-	@Test
-	public void testVerifyMethodWrongString(){
-		
-		SudokuVerifier verifier = setupVerifier();
-		
-		assertEquals(-2, verifier
-				.verify("123456789912345678891234567789123456678912345567891234456789123345678912234567891"));
-		
-	}*/
-	
 	@Test 
 	public void testVerifyMethodNegativeNumbers(){
 		
@@ -35,6 +24,8 @@ public class TestSudokuVerifier {
 		assertEquals(-1, 
 				verifier.verify("-417369825632158947958724316825437169791586432346912758289643571573291684164875293"));
 	}
+	
+	
 	
 	private SudokuVerifier setupVerifier(){
 		
