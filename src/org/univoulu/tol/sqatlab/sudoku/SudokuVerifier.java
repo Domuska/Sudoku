d9d94bdb0cb3a25d98adc79427765fccc6acabe2
somename <someemail>
2015-10-02 12:12:40 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,8 +3,18 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
+		
+		
 		// returns 1 if the candidate solution is correct
-		/*Return -1: means it is violating Rule #1
+		/*
+		
+    R1: A cell in a Sudoku game can only store positive digits, i.e. 1...9.
+    R2: All digits appear only once in a sub-grid, i.e. they cannot repeat.
+    R3: A digit can appear only once in the rows of the global grid.
+    R4: A digit can appear only once in the columns of the global grid.
+
+		
+		Return -1: means it is violating Rule #1
 		Return -2: means it is violating Rule #2
 		Return -3: means it is violating Rule #3
 		Return -4: means it is violating Rule #4
@@ -14,16 +24,11 @@ public class SudokuVerifier {
 		String row = candidateSolution.substring(0, 9);
 		System.out.println(row);
 		
-		/*
-		if(Integer.parseInt(candidateSolution) < 0){
-			
+		if(Integer.parseInt(row) < 0){
 			return -1;
 		}
-		else{
-			return 1;
-		}
-			*/
 		
+				
 		return 0;
 	}
 }
