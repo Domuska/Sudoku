93688f971c7f21f21b6cc6cb065b4f71e658f048
somename <someemail>
2015-10-02 12:04:39 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -13,13 +13,13 @@ public class SudokuVerifier {
 		System.out.println(candidateSolution);
 		//System.out.println(Long.parseLong(candidateSolution));
 		
-		/*if(Long.parseLong(candidateSolution) < 0){
+		if(Integer.parseInt(candidateSolution) < 0){
 			
 			return -1;
 		}
-		else{*/
+		else{
 			return 1;
-		//}
+		}
 			
 		
 		
