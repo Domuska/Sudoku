1e3c264f1402269b886f2f59465f775251d812cb
somename <someemail>
2015-10-02 11:49:21 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -19,6 +19,10 @@ public class TestSudokuVerifier {
 	@Test
 	public void testVerifyMethodWrongString(){
 		
+		SudokuVerifier verifier = setupVerifier();
+		
+		assertEquals(-2, verifier
+				.verify("123456789912345678891234567789123456678912345567891234456789123345678912234567891"));
 		
 	}
 	
