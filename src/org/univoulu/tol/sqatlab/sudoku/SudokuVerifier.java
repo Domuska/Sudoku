3102d015f9a8ea8557da7a665ce84137408cfbe1
somename <someemail>
2015-10-02 12:01:55 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -10,7 +10,8 @@ public class SudokuVerifier {
 		Return -4: means it is violating Rule #4
 		return -1 if the length is too short*/
 		
-		System.out.println(Long.parseLong(candidateSolution));
+		System.out.println(candidateSolution);
+		//System.out.println(Long.parseLong(candidateSolution));
 		
 		if(Integer.parseInt(candidateSolution) < 0){
 			
